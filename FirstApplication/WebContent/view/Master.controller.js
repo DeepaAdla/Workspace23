d0a6cf79c1bf9699c62a07321bac54554ae52609
BPSH023 <BPSH023@192.168.1.56>
2017-09-04 16:27:57 +0530

    commit4.1

--- FirstApplication/WebContent/view/Master.controller.js
+++ FirstApplication/WebContent/view/Master.controller.js
@@ -7,9 +7,10 @@ sap.ui.controller("sap.ui.demo.view.Master", {
 * @memberOf view.Master
 */
 	onInit: function() {
-           //Test 04Sept2017
+           //Test 04Sept2017 
            alert("Hello");
 	   alert("test1");
+	   alert("tes2");
 	},
 
 /**
