51d0dbbda1440332036fc46e96d40260d04cabc7
tuljathakur <31502196+tuljathakur@users.noreply.github.com>
2017-09-07 19:22:14 +0530

    commit7.6

--- FirstApplication/WebContent/view/Detail.controller.js
+++ FirstApplication/WebContent/view/Detail.controller.js
@@ -6,9 +6,9 @@ sap.ui.controller("sap.ui.demo.view.Detail", {
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf view.Detail
 */
-//	onInit: function() {
-//
-//	},
+	onInit: function() {
+           alert("Test1");
+	},
 
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
